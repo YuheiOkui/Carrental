d7799d13f3c9e1e5d2b80aae70838c966e7a8498
3030671 <3030671@30306-71>
2023-07-05 09:07:32 +0900

    commit1_kuro

--- src/main/java/carrental/domain/repository/user/Userrepository.java
+++ src/main/java/carrental/domain/repository/user/Userrepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import carrental.domain.model.User;
 
 //ユーザーリポジトリー
-public interface Userrepository extends JpaRepository<User,Integer>{
+public interface Userrepository extends JpaRepository<User,String>{
 
 }
